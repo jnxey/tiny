7737f1c8ccd2e403388aff577845940707784209
lee <846046245@qq.com>
2024-05-04 14:38:45 +0800

    feat: 处理package.json

--- scripts/build.js
+++ scripts/build.js
@@ -4,12 +4,15 @@ import alias from '@rollup/plugin-alias';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/tiny.js',
     format: 'umd',
     name: 'Tiny'
   },
   plugins: [
-    typescript(),
+    typescript({
+      declaration: true,
+      declarationDir: 'dist/types'
+    }),
     alias({
       entries: [{ find: '@', replacement: '../src' }]
     })
