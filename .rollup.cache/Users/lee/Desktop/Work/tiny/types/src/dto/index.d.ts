097fe2119c49d5f4b47ea094a0b7453be71ea10b
lee <846046245@qq.com>
2024-05-06 23:05:24 +0800

    feat: 处理打包相关

--- .rollup.cache/Users/lee/Desktop/Work/tiny/types/src/dto/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { DtoInput } from '@/types/dto';
-export declare class Dto {
-    code: string | number;
-    result: any;
-    msg?: string;
-    constructor({ code, result, msg }: DtoInput);
-}
